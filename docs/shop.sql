0c82c8c3b86056d83e472cb6b2a637c50bf1f3d4
mengxiangchen <mengxiangchen@cmcm.com>
2023-11-28 15:06:21 +1300

    edit doc

--- docs/shop.sql
+++ docs/shop.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50739
  File Encoding         : 65001
 
- Date: 27/11/2023 19:09:43
+ Date: 28/11/2023 15:00:28
 */
 
 SET NAMES utf8mb4;
@@ -36,7 +36,7 @@ CREATE TABLE `carts` (
   KEY `product_id` (`product_id`),
   CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
   CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `Products` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COMMENT='Table for storing cart information and details';
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COMMENT='Table for storing cart information and details';
 
 -- ----------------------------
 -- Table structure for orders
@@ -53,7 +53,7 @@ CREATE TABLE `orders` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Created Time',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Updated Time',
   PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for products
@@ -85,3 +85,9 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='Table for storing user information';
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `created_at`, `updated_at`) VALUES (1, 'user1', 'e10adc3949ba59abbe56e057f20f883e', 0, '2023-11-25 13:30:29', '2023-11-25 13:30:29');
+INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `created_at`, `updated_at`) VALUES (2, 'admin1', 'e10adc3949ba59abbe56e057f20f883e', 1, '2023-11-25 13:40:27', '2023-11-25 13:40:27');
+INSERT INTO `products` (`product_id`, `sku`, `name`, `description`, `price`, `created_at`, `updated_at`) VALUES (1, 'XL', 'test1_name', 'test description', 100.00, '2023-11-25 14:40:08', '2023-11-28 15:01:51');
+INSERT INTO `products` (`product_id`, `sku`, `name`, `description`, `price`, `created_at`, `updated_at`) VALUES (2, 'M,M1,M2', 'test2_name', 'test2_desc', 50.00, '2023-11-25 14:40:37', '2023-11-27 11:44:22');
+INSERT INTO `products` (`product_id`, `sku`, `name`, `description`, `price`, `created_at`, `updated_at`) VALUES (3, 'XL,XL1,XL2', 'test3_name', 'test3_desc', 20.00, '2023-11-25 14:41:03', '2023-11-27 12:27:26');
